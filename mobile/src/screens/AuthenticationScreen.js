0105e1d4d8aafe2d4d9a599b704c359ae4c21f1b
Dan Njoku <dannjoku1@gmail.com>
2018-07-10 21:10:42 -0400

    Authentication Screen
    authentication screen with login and signup buttons

--- mobile/src/screens/AuthenticationScreen.js
+++ mobile/src/screens/AuthenticationScreen.js
@@ -1,16 +1,75 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
+import Touchable from '@appandflow/touchable'
 
-const Root = styled.View``;
+const Root = styled.View`
+  flex: 1;
+  backgroundColor: ${props => props.theme.SECONDARY};
+  position: relative;  
+`;
 
-const T = styled.Text``;
+const ButtonLoginText = styled.Text`
+  color: ${props => props.theme.WHITE};
+  fontWeight: bold;
+  fontSize: 20;  
+`;
+
+const ButtonLogin = styled(Touchable).attrs({
+  feedback: 'opacity'
+})`
+  height: 75;
+  width: 150;
+  backgroundColor: ${props => props.theme.PRIMARY};
+  justifyContent: center;
+  alignItems: center;
+  position: absolute;
+  top: 40%
+  right: 110;
+  borderRadius: 20;
+  `;
+
+  const BottomTextContainer = styled.View`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0; 
+    height: 200;
+    justifyContent: center;
+    alignItems: center;
+  `;
+
+  const Button = styled(Touchable).attrs({
+    feedback: 'opacity',
+  })`
+    justifyContent: center;
+    alignItems: center;
+  `;
+
+  const ButtonText = styled.Text`
+  color: ${props => props.theme.WHITE};
+  fontWeight: 400;
+  fontSize: 15;  
+`;
+
+
 
 class AuthenticationScreen extends Component {
   state = { }
   render() {
     return (
       <Root>
-        <T>AuthenticationScreen</T>
+        <ButtonLogin>
+          <ButtonLoginText>
+            Sign Up
+          </ButtonLoginText>
+        </ButtonLogin>
+        <BottomTextContainer>
+          <Button>
+            <ButtonText>
+              Already have an account?
+            </ButtonText>
+          </Button>
+        </BottomTextContainer>
       </Root>
     );
   }
