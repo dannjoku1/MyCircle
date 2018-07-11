05c5a30879efbbe527a097683c4587e92d88f143
Dan Njoku <dannjoku1@gmail.com>
2018-07-10 20:15:01 -0400

    Authentication
    Added user authentication boilerplate to navigations

--- mobile/src/navigations.js
+++ mobile/src/navigations.js
@@ -8,7 +8,7 @@ import HomeScreen from './screens/HomeScreen';
 import ExploreScreen from './screens/ExploreScreen';
 import NotificationsScreen from './screens/NotificationsScreen';
 import ProfileScreen from './screens/ProfileScreen';
-//import AuthenticationScreen from './screens/AuthenticationScreen';
+import AuthenticationScreen from './screens/AuthenticationScreen';
 //import NewTweetScreen from './screens/NewTweetScreen';
 
 import { colors } from './utils/constants' 
@@ -105,12 +105,16 @@ class AppNavigator extends Component {
       dispatch: this.props.dispatch,
       state: this.props.nav,
     })
+    if (!this.props.user.isAuthenticated) {
+      return <AuthenticationScreen />
+    }
     return <AppMainNav navigation={nav} />
   }
 }
 
 export default connect(state => ({
-  nav: state.nav
+  nav: state.nav,
+  user: state.user
 }))(AppNavigator);
 
 export const router = AppMainNav.router;
