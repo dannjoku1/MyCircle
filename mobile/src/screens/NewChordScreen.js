c6147b3de5532c518ae836921baf4610a76b3834
Dan Njoku <dannjoku1@gmail.com>
2018-07-16 06:59:50 -0400

    additional chord component

--- mobile/src/screens/NewChordScreen.js
+++ mobile/src/screens/NewChordScreen.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
+import { Platform, Keyboard } from 'react-native';
+import Touchable from '@appandflow/touchable';
+import { graphql, compose } from 'react-apollo';
+import { connect } from 'react-redux';
+
+import { colors } from '../utils/constants';
 
 const Root = styled.View`
   backgroundColor: ${props => props.theme.WHITE};
   flex: 1;
   alignItems: center;
+`;
+
+const Wrapper = styled.View`
+  height: 80%;
+  width: 90%;
+  paddingTop: 5;
+  position: relative;
+`;
+
+const Input = styled.TextInput.attrs({
+  multiline: true,
+  placeholder: "What's happening?",
+  maxLength: 140,
+  selectionColor: Platform.OS === 'ios' && colors.PRIMARY,
+  autoFocus: true,
+})`
+  height: 40%;
+  width: 100%;
+  fontSize: 18;
+  color: ${props => props.theme.SECONDARY};
 `;
 
 const T = styled.Text``
@@ -14,7 +40,9 @@ class NewChordScreen extends Component {
   render() {
     return (
       <Root>
-        <T>Profile</T>
+        <Wrapper>
+          <Input />
+        </Wrapper>
       </Root>
     );
   }
